14cd8c65e86e3f46e276372f8396601a9da35daf
nathan-real <nadelmard@ensc.fr>
2025-05-07 09:38:24 +0200

    Refonte de parcelle et des sous classes

--- Parcelle.cs
+++ Parcelle.cs
@@ -5,8 +5,14 @@ public abstract class Parcelle
     public int Hauteur { get; set; }
     public Cepage?[,] MatriceEtat { get; set; }
     public bool EstBio { get; set; } = false;
+    public string? TypeSol { get; set; }
+    public int Fertilite { get; set; } // 0 à 100
+    public int Humidite { get; set; } // 0 à 100
+    public int Ensoleillement { get; set; } // 0 à 100 (Ensoleillement actuelle, qui changera via la météo)
+    public int Temperature { get; set; } // température actuelle
+    //public List<MauvaiseHerbe> MauvaisesHerbes { get; set; } = new(); // en cas de futur ajout
 
-    protected Parcelle(string nom, int largeur, int hauteur)
+    protected Parcelle(string nom, int largeur, int hauteur, string typeSol)
     {
         Nom = nom;
         Largeur = largeur;
--- ParcelleArgileuse.cs
+++ ParcelleArgileuse.cs
@@ -1,8 +1,12 @@
 public class ParcelleArgileuse : Parcelle
 {
-    public ParcelleArgileuse(string nom, int largeur, int hauteur) : base(nom, largeur, hauteur)
+    public ParcelleArgileuse(string nom, int largeur, int hauteur)
+        : base(nom, largeur, hauteur, "argileux")
     {
-
+        // Paramètres typiques d'un sol argileux
+        Fertilite = 80;         // Très fertile
+        Humidite = 75;          // Forte rétention d’eau
+        Ensoleillement = 50;    // Drainage lent = ensoleillement souvent réduit
+        Temperature = 18;       // Température modérée (sol plus lent à chauffer)
     }
 }
-
